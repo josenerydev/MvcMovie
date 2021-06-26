296a5cae8d7adc0eba717fa1c484bf4467341a78
josenerydev <josenerydev@gmail.com>
2021-06-25 22:13:59 -0300

    Initial Commit

---
+++ src/MvcMovie.Web/Data/MvcMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Web.Models;
+
+namespace MvcMovie.Web.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Web.Models.Movie> Movie { get; set; }
+    }
+}
